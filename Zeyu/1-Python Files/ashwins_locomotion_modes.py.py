38c4ae635ba9c87b913dec5c9aa0a5c81dfaea62
Zeyu Lu <11510076@mail.sustc.edu.cn>
2018-09-30 10:09:19 +0800

    I exported those python files from your Jupyter notebook for debug. I prefer to debug in Pycharm IDE

---
+++ Zeyu/1-Python Files/ashwins_locomotion_modes.py.py
@@ -0,0 +1,184 @@
+
+# coding: utf-8
+
+# In[15]:
+
+
+# Imports
+import numpy as np
+from scipy.io import loadmat
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, Activation, LSTM, SimpleRNN, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Flatten
+import keras.backend as K
+import matplotlib.pyplot as plt
+from keras.preprocessing.sequence import TimeseriesGenerator
+from keras.utils import Sequence, to_categorical
+
+
+# ## Import the IMU Data
+
+# In[2]:
+
+
+# Import the data from the mat file
+
+data_in = loadmat('walk_data_ashwin.mat')
+X = data_in['imu_features']
+Y = data_in['lm_label']/300
+Ytemp = np.argwhere(Y)[:, 1]
+Yint = np.zeros(Ytemp.shape)
+for i in range(Ytemp.shape[0]):
+    if Ytemp[i] == 1:
+        Yint[i] = 0
+    elif Ytemp[i] == 4:
+        Yint[i] = 1
+    elif Ytemp[i] == 5:
+        Yint[i] = 2
+            
+Y_prep = to_categorical(Yint)
+print(Y_prep)
+
+# Get some statistics from the data
+exp_dur = X.shape[0]/100.0
+nfeat = X.shape[1]
+t = np.linspace(0, exp_dur, X.shape[0])
+
+# Plot parts of the data
+plt.figure(figsize=(10, 6))
+plt.plot(t, X[:, 56])
+plt.plot(t, Y*300)
+plt.xlim([6, 12])
+plt.ylabel('Features')
+plt.xlabel('Time (s)')
+plt.show()
+
+
+# ## Preprocess the data
+
+# In[3]:
+
+
+scale_factors = np.std(X, axis=0)
+mean_values = np.mean(X, axis=0)
+X_prep = (X - mean_values)/scale_factors
+#print("After normalizing: ")
+#print("Mean is: ", np.mean(X_prep, axis=0))
+#print("Stdev. is: ", np.std(X_prep, axis=0))
+plt.figure(figsize=(10, 6))
+plt.plot(t, X_prep)
+plt.plot(t, Y)
+plt.xlim([6, 12])
+plt.ylabel('Features')
+plt.xlabel('Time (s)')
+plt.show()
+
+
+# ## Trying Logistic Regression
+
+# In[4]:
+
+
+lr_model = Sequential()
+lr_model.add(Dense(32, input_dim=nfeat, activation='relu'))
+lr_model.add(Dense(3))
+lr_model.add(Activation('softmax'))
+lr_model.compile(optimizer='adam',
+                 loss='categorical_crossentropy',
+                 metrics=['accuracy'])
+lr_model.fit(X_prep, Y_prep, epochs=30, batch_size=64)
+
+
+# ### Plot the labels
+# 
+
+# In[5]:
+
+
+preds = lr_model.predict(X_prep)
+print(preds)
+plt.figure(figsize=(15, 9))
+plt.plot(t, X[:, 56], label='Right Foot Tilt')
+plt.plot(t, X[:, 57], label='Right Shank Tilt')
+plt.plot(t, X[:, 58], label='Right Thigh Tilt')
+plt.plot(t, preds[:, 0]*40, label='Standing')
+plt.plot(t, preds[:, 1]*40, label='Walking')
+plt.plot(t, preds[:, 2]*40, label='Unknown')
+plt.xlim([26, 46])
+plt.ylim([-50, 50])
+plt.ylabel('Features')
+plt.xlabel('Time (s)')
+plt.title('Locomotion Mode Prediction Using Logistic Regression')
+plt.grid()
+plt.legend()
+plt.show()
+
+
+# ## Using CNNs
+
+# ### CNN Specific Pre-Processing of the data
+
+# In[23]:
+
+
+# Function to split the data and labels into windows that are causally aligned.
+def get_sub_sequences(data_array, y_array, window_size=120, step_size=90, dims=None, seq_out=False, causal=True):
+    rows = data_array.shape[0]
+    cols = data_array.shape[1]
+
+    if dims == None:
+        outdims = [i for i in range(cols)]
+    else:
+        outdims = dims
+        
+    sequences = rows//step_size
+    out_x = np.zeros((sequences, window_size, len(outdims)))
+    if seq_out:
+        out_y = np.zeros((sequences, window_size, y_array.shape[1]))
+    else:
+        out_y = np.zeros((sequences, y_array.shape[1]))
+        
+    idxs = range(window_size, rows, step_size)    
+    
+    for i, j in enumerate(idxs):
+        out_x[i, :, :] = data_array[j-window_size:j, outdims]
+        if seq_out:
+            out_y[i, :, :] = y_array[j-window_size:j, :]
+        else:
+            out_y[i, :] = y_array[j, :]
+    
+    return out_x, out_y
+
+# Generate dataset of sub-sequences
+X_seq, Y_seq = get_sub_sequences(X_prep, Y_prep, window_size=120, step_size=1)
+X_seq = np.reshape(X_seq, newshape=(X_seq.shape[0], X_seq.shape[1], X_seq.shape[2], 1))
+# Plot some subsequences to make sure that we're doing it right
+# plt.figure(figsize=(15, 9))
+# for i in range(1, 10, 1):
+#     plt.subplot(3, 3, i)
+#     plt.plot(X_seq[i*2+590, :, 56:59])
+#     plt.title('Subsequence from {}, LM={}'.format(i*2+590, Y_seq[i]))
+#     plt.xlabel('Sample')
+#     plt.ylabel('Value')
+#     plt.grid()
+
+# plt.show()
+
+
+# ### Model 1
+
+# In[25]:
+
+
+cnn_model1 = Sequential()
+cnn_model1.add(Conv2D(filters=16, kernel_size=9, input_shape=(120, 63, 1), 
+                      data_format='channels_last', activation='relu', padding='valid'))
+cnn_model1.add(MaxPooling2D(pool_size=(10, 3), padding='valid'))
+cnn_model1.add(Flatten())
+cnn_model1.add(Dense(3, activation='softmax'))
+#cnn_model1.add(Dense(3, activation='softmax'))
+cnn_model1.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+cnn_model1.summary()
+print("X_seq: ", X_seq.shape)
+print("Y_seq: ", Y_seq.shape)
+cnn_model1.fit(X_seq, Y_seq, batch_size=128)
+
