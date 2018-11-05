0b971f7e09ac96c56f10e7d3a649900cc1b1eaff
ZeyuLu <11510076@mail.sustc.edu.cn>
2018-11-05 22:32:12 +0800

    Tired several  hypermaras in rnn LSTM

---
+++ Zeyu/1-Python Files/Main/Test-seriesdataintoRNN.py
@@ -0,0 +1,41 @@
+import model
+import matplotlib.pyplot as plt
+
+Onlinedata = model.DATA(ReadALLSubjs=False, SplitData=True)
+Onlinedata.scandata(Subj=[1])
+Onlinedata.stackdata(ChooseSensors=['IMU'])
+Onlinedata.PreprocessData()
+Onlinedata.TRAIN_DATA_all.shape
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+model_rnn5 = Sequential()
+model_rnn5.add(LSTM(512, input_shape=(None, Onlinedata.TRAIN_DATA_all.shape[1]),
+                    dropout=0.5))
+model_rnn5.add(Dense(Onlinedata.TRAIN_LABEL_all.shape[1], activation='softmax'))
+model_rnn5.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model_rnn5.summary()
+TenEpochesWithDropout = model_rnn5.fit(
+    Onlinedata.TRAIN_DATA_all.reshape((Onlinedata.TRAIN_DATA_all.shape[0], 1, Onlinedata.TRAIN_DATA_all.shape[1])),
+    Onlinedata.TRAIN_LABEL_all, epochs=10,
+    validation_data=(
+        Onlinedata.VAL_DATA_all.reshape((Onlinedata.VAL_DATA_all.shape[0], 1, Onlinedata.VAL_DATA_all.shape[1])),
+        Onlinedata.VAL_LABEL_all))
+model_rnn5.save('../ModelLogs/RNN-10Epochs-LSTM(512)-dropout(0.5)-AbsMax.h5')
+
+plt.figure(1)
+plt.plot(TenEpochesWithDropout.history['loss'], label='train_loss')
+plt.plot(TenEpochesWithDropout.history['val_loss'], label='val_loss')
+plt.title('RNN-10Epochs-LSTM(512)-Dropout(0.5)')
+plt.legend()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+plt.figure(2)
+plt.plot(TenEpochesWithDropout.history['acc'], label='train_acc')
+plt.plot(TenEpochesWithDropout.history['val_acc'], label='val_acc')
+plt.title('RNN-10epoches-LSTM(512)-Dropout(0.5)')
+plt.legend()
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.show()
